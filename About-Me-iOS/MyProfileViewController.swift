c1bd3ebb0e4a739305c8abee2c3dba12d0b2aaea
Do-hyun-Kim <dohyun8032@gmail.com>
2021-05-01 01:02:22 +0900

    MyProfileTabBar Animation 추가 및 CollectionVIew Extension

--- About-Me-iOS/MyProfileViewController.swift
+++ About-Me-iOS/MyProfileViewController.swift
@@ -19,6 +19,13 @@ class MyProfileViewController: UIViewController {
     @IBOutlet weak var myProfileCharacterTagSecond: UIButton!
     @IBOutlet weak var myProfileCharacterTagThird: UIButton!
     @IBOutlet weak var myProfileContentContainerView: UIView!
+    @IBOutlet weak var myProfileMyAnswerButton: UIButton!
+    @IBOutlet weak var myProfileMyLikeButton: UIButton!
+    @IBOutlet weak var myProfileMyScrapButton: UIButton!
+    @IBOutlet weak var myProfileBottomLineView: UIView!
+    @IBOutlet weak var myProfileCollectionView: UICollectionView!
+    var myProfileBottomLineViewLeadingConstraint:NSLayoutConstraint!
+    var myProfileBottomLineViewWidthConstraint: NSLayoutConstraint!
     override func viewDidLoad() {
         super.viewDidLoad()
         self.setInitLayout()
@@ -26,7 +33,6 @@ class MyProfileViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         self.navigationController!.navigationBar.titleTextAttributes = [NSAttributedString.Key.font: UIFont(name: "AppleSDGothicNeo-Regular", size: 18)!,NSAttributedString.Key.foregroundColor : UIColor.white]
     }
-    
     
     
     private func setInitLayout() {
@@ -39,6 +45,10 @@ class MyProfileViewController: UIViewController {
         self.navigationItem.rightBarButtonItem = rightBarButtonItem
         self.navigationController!.navigationBar.titleTextAttributes = [NSAttributedString.Key.font: UIFont(name: "AppleSDGothicNeo-Regular", size: 18)!,NSAttributedString.Key.foregroundColor : UIColor.white]
         self.navigationItem.title = "MY"
+        let nib = UINib(nibName: "MyProfileCollectionViewCell", bundle: nil)
+        self.myProfileCollectionView.register(nib, forCellWithReuseIdentifier: "MyProfileCell")
+        self.myProfileCollectionView.delegate = self
+        self.myProfileCollectionView.dataSource = self
         self.myProfileContainerView.backgroundColor = UIColor(red: 226/255, green: 49/255, blue: 34/255, alpha: 1.0)
         self.myProfileBoxView.layer.cornerRadius = 15
         self.myProfileBoxView.layer.masksToBounds = false
@@ -66,10 +76,110 @@ class MyProfileViewController: UIViewController {
         self.myProfileCharacterTagThird.backgroundColor = UIColor(red: 238/255, green: 238/255, blue: 238/255, alpha: 1.0)
         self.myProfileContentContainerView.layer.cornerRadius = 25
         self.myProfileContentContainerView.layer.masksToBounds = true
-        
+        self.myProfileMyAnswerButton.isSelected = true
+        self.myProfileMyAnswerButton.setTitle("내가 한 답", for: .selected)
+        self.myProfileMyAnswerButton.titleLabel?.font = UIFont(name: "GmarketSans-Medium", size: 14)
+        self.myProfileMyAnswerButton.titleLabel?.textAlignment = .center
+        self.myProfileMyAnswerButton.setTitleColor(UIColor(red: 0/255, green: 0/255, blue: 0/255, alpha: 1.0), for: .selected)
+        self.myProfileMyLikeButton.setTitle("좋아요", for: .normal)
+        self.myProfileMyLikeButton.titleLabel?.font = UIFont(name: "GmarketSans-Medium", size: 14)
+        self.myProfileMyLikeButton.titleLabel?.textAlignment = .center
+        self.myProfileMyLikeButton.setTitleColor(UIColor(red: 85/255, green: 85/255, blue: 85/255, alpha: 1.0), for: .normal)
+        self.myProfileMyScrapButton.setTitle("스크랩", for: .normal)
+        self.myProfileMyScrapButton.titleLabel?.font = UIFont(name: "GmarketSans-Medium", size: 14)
+        self.myProfileMyScrapButton.titleLabel?.textAlignment = .center
+        self.myProfileMyScrapButton.setTitleColor(UIColor(red: 85/255, green: 85/255, blue: 85/255, alpha: 1.0), for: .normal)
+        self.myProfileBottomLineView.translatesAutoresizingMaskIntoConstraints = false
+        self.myProfileBottomLineView.backgroundColor = UIColor(red: 34/255, green: 34/255, blue: 34/255, alpha: 1.0)
+        self.myProfileBottomLineViewWidthConstraint = self.myProfileBottomLineView.widthAnchor.constraint(equalToConstant: 62)
+        self.myProfileBottomLineViewWidthConstraint.isActive = true
+        self.myProfileBottomLineView.heightAnchor.constraint(equalToConstant: 2).isActive = true
+        self.myProfileBottomLineViewLeadingConstraint = self.myProfileBottomLineView.leadingAnchor.constraint(equalTo: self.myProfileMyAnswerButton.leadingAnchor, constant: 25)
+        self.myProfileBottomLineViewLeadingConstraint.isActive = true
+        self.myProfileBottomLineView.bottomAnchor.constraint(equalTo: self.myProfileMyAnswerButton.bottomAnchor).isActive = true
+        self.myProfileMyLikeButton.addTarget(self, action: #selector(self.didTapMyLikeButton(_:)), for: .touchUpInside)
+        self.myProfileMyScrapButton.addTarget(self, action: #selector(self.didTapMyScrapButton(_:)), for: .touchUpInside)
+        self.myProfileMyAnswerButton.addTarget(self, action: #selector(self.didTapMyAnswerButton(_:)), for: .touchUpInside)
     }
     
+    @objc
+    private func didTapMyLikeButton(_ sender: UIButton) {
+        
+        UIView.animate(withDuration: 0.5, delay: 1.0, usingSpringWithDamping: 1.0, initialSpringVelocity: 1.0, options: .curveLinear) {
+            
+            if self.myProfileBottomLineView.frame.origin.x > self.myProfileMyLikeButton.frame.origin.x {
+                self.myProfileBottomLineView.layoutIfNeeded()
+                let transition = CATransition()
+                transition.duration = 0.1
+                transition.type = CATransitionType(rawValue: "push")
+                transition.subtype = .fromRight
+                self.myProfileBottomLineView.layer.add(transition, forKey: kCATransition)
+                self.myProfileBottomLineViewLeadingConstraint.constant = self.myProfileMyLikeButton.frame.minX - 15
+                self.myProfileBottomLineView.updateConstraints()
+            } else {
+                self.myProfileBottomLineView.layoutIfNeeded()
+                let transition = CATransition()
+                transition.duration = 0.1
+                transition.type = CATransitionType(rawValue: "push")
+                transition.subtype = .fromLeft
+                self.myProfileBottomLineView.layer.add(transition, forKey: kCATransition)
+                self.myProfileBottomLineViewLeadingConstraint.constant = self.myProfileMyLikeButton.frame.minX - 15
+                self.myProfileBottomLineView.updateConstraints()
+            }
+        } completion: { success in
+            if success == true {
+                self.myProfileBottomLineView.layer.removeAnimation(forKey: "push")
+            }
+        }
+    }
     
+    @objc
+    private func didTapMyScrapButton(_ sender: UIButton) {
+        UIView.animate(withDuration: 0.5, delay: 1.0, usingSpringWithDamping: 1.0, initialSpringVelocity: 1.0, options: .curveLinear) {
+            self.myProfileBottomLineView.layoutIfNeeded()
+            let transition = CATransition()
+            transition.duration = 0.1
+            transition.type = CATransitionType(rawValue: "push")
+            transition.subtype = .fromLeft
+            self.myProfileBottomLineView.layer.add(transition, forKey: kCATransition)
+            self.myProfileBottomLineViewLeadingConstraint.constant = self.myProfileMyScrapButton.frame.minX - 15
+            self.myProfileBottomLineView.updateConstraints()
+        } completion: { success in
+            if success == true {
+                self.myProfileBottomLineView.layer.removeAnimation(forKey: "push")
+            }
+        }
+    }
+    
+    @objc
+    private func didTapMyAnswerButton(_ sender: UIButton) {
+        UIView.animate(withDuration: 0.5, delay: 1.0, usingSpringWithDamping: 1.0, initialSpringVelocity: 1.0, options: .curveLinear) {
+            if self.myProfileBottomLineView.frame.origin.x > self.myProfileMyAnswerButton.frame.origin.x {
+                self.myProfileBottomLineView.layoutIfNeeded()
+                let transition = CATransition()
+                transition.duration = 0.1
+                transition.type = CATransitionType(rawValue: "push")
+                transition.subtype = .fromRight
+                self.myProfileBottomLineView.layer.add(transition, forKey: kCATransition)
+                self.myProfileBottomLineViewLeadingConstraint.constant = self.myProfileMyAnswerButton.frame.minX - 15
+                self.myProfileBottomLineView.updateConstraints()
+            } else {
+                self.myProfileBottomLineView.layoutIfNeeded()
+                let transition = CATransition()
+                transition.duration = 0.1
+                transition.type = CATransitionType(rawValue: "push")
+                transition.subtype = .fromLeft
+                self.myProfileBottomLineView.layer.add(transition, forKey: kCATransition)
+                self.myProfileBottomLineViewLeadingConstraint.constant = self.myProfileMyAnswerButton.frame.minX - 15
+                self.myProfileBottomLineView.updateConstraints()
+            }
+        } completion: { success in
+            if success == true {
+                self.myProfileBottomLineView.layer.removeAnimation(forKey: "push")
+            }
+        }
+
+    }
     
     @objc
     public func didTapdetailPushButton() {
@@ -78,4 +188,29 @@ class MyProfileViewController: UIViewController {
         guard let profileDetailVC = profileDetailView else { return }
         self.navigationController?.pushViewController(profileDetailVC, animated: true)
     }
+}
+
+
+extension MyProfileViewController : UICollectionViewDelegate,UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let myProfileCell = collectionView.dequeueReusableCell(withReuseIdentifier: "MyProfileCell", for: indexPath) as? MyProfileCollectionViewCell
+        myProfileCell?.myProfileContentTitleLabel.text = "# 열정충만"
+        myProfileCell?.myProfileQuestionTitleLabel.text = "Q. 당신에게 가족은 어떤 의미인가요?"
+        myProfileCell?.myProfileContentDateLabel.text = "2021.03.19"
+        
+        return myProfileCell!
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 335, height: 100)
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+    }
+    
+    
 }
---
+++ About-Me-iOS/ProfileXib/MyProfileCollectionViewCell.swift
@@ -0,0 +1,37 @@
+//
+//  MyProfileCollectionViewCell.swift
+//  About-Me-iOS
+//
+//  Created by Kim dohyun on 2021/05/01.
+//
+
+import UIKit
+
+class MyProfileCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var myProfileListContentView: UIView!
+    @IBOutlet weak var myProfileContentTitleLabel: UILabel!
+    @IBOutlet weak var myProfileContentDateLabel: UILabel!
+    @IBOutlet weak var myProfileQuestionTitleLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        self.setInitLayout()
+    }
+
+    private func setInitLayout() {
+        self.myProfileListContentView.layer.borderWidth = 1
+        self.myProfileListContentView.layer.borderColor = UIColor(red: 221/255, green: 221/255, blue: 221/255, alpha: 1.0).cgColor
+        self.myProfileListContentView.layer.cornerRadius = 8
+        self.myProfileListContentView.layer.masksToBounds = true
+        self.myProfileQuestionTitleLabel.textColor = UIColor(red: 34/255, green: 34/255, blue: 34/255, alpha: 1.0)
+        self.myProfileQuestionTitleLabel.font = UIFont(name: "AppleSDGothicNeo-Regular", size: 15)
+        self.myProfileQuestionTitleLabel.textAlignment = .left
+        self.myProfileContentDateLabel.textColor = UIColor(red: 153/255, green: 153/255, blue: 153/255, alpha: 1.0)
+        self.myProfileContentDateLabel.textAlignment = .left
+        self.myProfileContentDateLabel.font = UIFont(name: "AppleSDGothicNeo-Regular", size: 12)
+        self.myProfileContentTitleLabel.textColor = UIColor(red: 255/255, green: 98/255, blue: 98/255, alpha: 1.0)
+        self.myProfileContentTitleLabel.textAlignment = .left
+        self.myProfileContentTitleLabel.font = UIFont(name: "AppleSDGothicNeo-Regular", size: 12)
+    }
+}
