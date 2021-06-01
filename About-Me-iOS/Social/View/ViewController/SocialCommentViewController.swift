bdc4b124e2a43dc78118b9984db417778badafee
Hyeonlee12 <hyeonlee9512@gmail.com>
2021-06-02 01:05:04 +0900

    comment

--- About-Me-iOS/Social/View/ViewController/SocialCommentViewController.swift
+++ About-Me-iOS/Social/View/ViewController/SocialCommentViewController.swift
@@ -17,10 +17,12 @@ class SocialCommentViewController: UIViewController {
     @IBOutlet weak var bottomView: UIView!
     @IBOutlet weak var commentTextField: UITextField!
     @IBOutlet weak var commentButton: UIButton!
+    @IBOutlet weak var bottomConstraint: NSLayoutConstraint!
     
     var panGestureRecognizer: UIPanGestureRecognizer?
     var originalPosition: CGPoint?
     var currentPositionTouched: CGPoint?
+    private var pullControl = UIRefreshControl()
     
     // MARK: - Lifecycle
     
@@ -34,6 +36,12 @@ class SocialCommentViewController: UIViewController {
     
     @IBAction func backButtonDidTap(_ sender: Any) {
         self.dismiss(animated: false, completion: nil)
+    }
+    
+    @objc private func refreshListData(_ sender: Any) {
+        // api
+        self.commentTableView.reloadData()
+        self.pullControl.endRefreshing()
     }
     
     @objc
@@ -77,7 +85,11 @@ class SocialCommentViewController: UIViewController {
         self.roundView.layer.cornerRadius = 10
         
         self.panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(panGestureAction(_:)))
-        self.commentTableView.addGestureRecognizer(panGestureRecognizer!)
+//        self.commentTableView.addGestureRecognizer(panGestureRecognizer!)
+        self.commentTableView.dataSource = self
+        self.commentTableView.delegate = self
+        self.pullControl.addTarget(self, action: #selector(refreshListData(_:)), for: .valueChanged)
+        self.commentTableView.refreshControl = pullControl
         
         self.bottomView.layer.borderWidth = 1
         self.bottomView.layer.borderColor = UIColor.whiteTwo.cgColor
@@ -85,6 +97,44 @@ class SocialCommentViewController: UIViewController {
         self.commentButton.layer.borderWidth = 1
         self.commentButton.layer.borderColor = UIColor.gray333.cgColor
         self.commentButton.layer.cornerRadius = 15
+        
+        let socialCommentNib = UINib(nibName: "SocialCommentCell", bundle: nil)
+        self.commentTableView.register(socialCommentNib, forCellReuseIdentifier: "socialCommentCell")
+        
+        NotificationCenter.default.addObserver(self, selector:#selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector:#selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
+    
+    // MARK: - Selectors
+    
+    @objc
+    private func keyboardWillShow(_ notification: Notification) {
+        if let keyboardFrame: NSValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue {
+            let keyboardHeight = keyboardFrame.cgRectValue.height
+            
+            self.bottomConstraint.constant = keyboardHeight
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    @objc
+    private func keyboardWillHide(_ notification: Notification) {
+        self.bottomConstraint.constant = 0
+        self.view.layoutIfNeeded()
+    }
+}
+
+extension SocialCommentViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "socialCommentCell", for: indexPath)
+        return cell
+    }
+}
+
+extension SocialCommentViewController: UITableViewDelegate {
     
 }
