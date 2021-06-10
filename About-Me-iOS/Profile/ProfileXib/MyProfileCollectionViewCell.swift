f790400c92c6fd53cf67f90e469f7c96efb5eb46
Do-hyun-Kim <dohyun8032@gmail.com>
2021-06-10 20:35:37 +0900

    Merge Confilct

--- About-Me-iOS/Profile/ProfileXib/MyProfileCollectionViewCell.swift
+++ About-Me-iOS/Profile/ProfileXib/MyProfileCollectionViewCell.swift
@@ -12,15 +12,11 @@ class MyProfileCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var myProfileListContentView: UIView!
     @IBOutlet weak var myProfileContentTitleLabel: UILabel!
     @IBOutlet weak var myProfileContentDateLabel: UILabel!
-    @IBOutlet weak var myProfileContentButton: UIButton!
+    @IBOutlet weak var myProfileContentImageView: UIImageView!
     @IBOutlet weak var myProfileQuestionTitleLabel: UILabel!
     @IBOutlet weak var myProfileAnswerTitleLabel: UILabel!
     @IBOutlet weak var myProfileAnswerImageView: UIImageView!
-    public var shareButtonClouser: (() -> ())?
-    public var likesButtonClouser: (() -> ())?
-    public var scrapButtonClouser: (() -> ())?
-    public var selectIndex = 0
-    public var CategoryFlag = ""
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
@@ -56,21 +52,5 @@ class MyProfileCollectionViewCell: UICollectionViewCell {
         self.myProfileContentTitleLabel.font = UIFont(name: "AppleSDGothicNeo-Regular", size: 12)
         self.myProfileAnswerImageView.image = UIImage(named: "slice")
         self.myProfileAnswerImageView.contentMode = .scaleToFill
-        self.myProfileContentButton.addTarget(self, action: #selector(didTapMyProfileShareButton(_:)), for: .touchUpInside)
-    }
-    
-    
-    @objc
-    public func didTapMyProfileShareButton(_ sender: UIButton) {
-        switch self.CategoryFlag {
-        case "answer":
-            shareButtonClouser!()
-        case "likes":
-            likesButtonClouser!()
-        case "scrap":
-            scrapButtonClouser!()
-        default:
-            break
-        }
     }
 }
