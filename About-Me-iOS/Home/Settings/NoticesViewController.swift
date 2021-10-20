f5145fc1b0c69694f028c1188b864ef44eddbf7c
Do-hyun-Kim <dohyun8032@gmail.com>
2021-10-20 22:28:08 +0900

    Revert "NotieViewController TableView 구현 or LoginStoryBoard AutoLayout 수정"
    This reverts commit 9eee1615968a1b71450249f808569acbebd179ac.

--- About-Me-iOS/Home/Settings/NoticesViewController.swift
+++ About-Me-iOS/Home/Settings/NoticesViewController.swift
@@ -9,17 +9,9 @@ import UIKit
 
 class NoticesViewController: UIViewController {
 
-    @IBOutlet weak var noticesTableView: UITableView!
-    var titleItem: [String] = ["개인정보 처리 방침 안내","서비스 이용 약관 안내","운영 정책 변경 안내","업데이트 소식"]
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        setLayoutInit()
-        // Do any additional setup after loading the view.
-    }
-    
-    
-    public func setLayoutInit(){
         let leftBarButtonItem = UIBarButtonItem(image: UIImage(named: "ArrowLeft"), style: .plain, target: self, action: #selector(noticeBackButtonDidTap))
         let navigationApp = UINavigationBarAppearance()
         navigationApp.configureWithTransparentBackground()
@@ -28,11 +20,7 @@ class NoticesViewController: UIViewController {
         self.navigationItem.title = "공지 사항"
         self.navigationController?.navigationBar.tintColor = .black
         self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.font: UIFont(name: "AppleSDGothicNeo-Medium", size: 18)!,NSAttributedString.Key.foregroundColor : UIColor.gray333]
-        noticesTableView.delegate = self
-        noticesTableView.dataSource = self
-        noticesTableView.separatorColor = .lineEee
-        noticesTableView.tableFooterView = UIView()
-        noticesTableView.isScrollEnabled = false
+        // Do any additional setup after loading the view.
     }
     
     
@@ -52,28 +40,4 @@ class NoticesViewController: UIViewController {
     }
     */
 
-}
-
-
-extension NoticesViewController: UITableViewDelegate,UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return titleItem.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let view = UIImageView(frame: CGRect(x: 0, y: 0, width: 20, height: 20))
-        view.image = UIImage(named: "rightblackArrow")
-        let cell = tableView.dequeueReusableCell(withIdentifier: "noticesCell", for: indexPath)
-        cell.textLabel?.text = titleItem[indexPath.row]
-        cell.textLabel?.textColor = .gray333
-        cell.textLabel?.font = UIFont(name: "AppleSDGothicNeo-Medium", size: 16)
-        cell.accessoryView = view
-        return cell
-    }
-    
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 60
-    }
-    
-    
 }
