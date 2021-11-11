49d9dd8daa9a670e141a84141e699483d70d4cfa
joongkyu-park <jk.park.tech@gmail.com>
2021-11-12 00:50:32 +0900

    Pod fix

--- About-Me-iOS/SceneDelegate.swift
+++ About-Me-iOS/SceneDelegate.swift
@@ -15,6 +15,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
         
+        
         // MARK: - RootViewController set
         if UserDefaults.standard.string(forKey: "USER_ID") != nil {
             if let windowScene = scene as? UIWindowScene {
